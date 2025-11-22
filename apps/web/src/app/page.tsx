b43caf3028e8a5f6fec173380924c5296bec58f9
Abdulrahman Nahhas <nahhas909@gmail.com>
2025-11-22 17:44:40 +0300

    feat: refactor web app with new structure and configs with new next.js 16 project.

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center text-5xl font-bold">
+      الصفحة الرئيسية
+    </div>
+  );
+};
+
+export default HomePage;
