a1e24dcc926bdeab6a9a8b09555c665864fdda14
Ahmederh <Ahmedfiraswitty@gmail.com>
2026-02-23 23:09:09 +0300

    add Empty

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
+import {
+  Empty,
+  EmptyContent,
+  EmptyHeader,
+  EmptyTitle,
+  EmptyDescription,
+} from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">العودة الئ الرئيسية</Link>
-    </div>
+    <Empty>
+      <EmptyHeader className="flex flex-wrap items-center mt-40">
+        <EmptyTitle> عذراً لم يتم ايجاد ما تبحث عنه</EmptyTitle>
+        <EmptyDescription>الصفحة التي تبحث عنها غير موجودة</EmptyDescription>
+        <Button className="bg-amber-600" variant="secondary" type="submit">
+          <Link href="/"> العودة الئ الرئيسية</Link>
+        </Button>
+      </EmptyHeader>
+    </Empty>
   );
 }
