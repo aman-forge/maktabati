6ed936cef966eb5fbf22e0b96e2104112e922bd3
Ahmederh <Ahmedfiraswitty@gmail.com>
2026-02-25 22:48:15 +0300

    final change

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -15,14 +15,22 @@ import { LinkBreakIcon } from "@phosphor-icons/react";
 export default function NotFound() {
   return (
     <Empty>
-      <EmptyHeader className="text-xl md:text-3xl lg:text-5xl h-screen flex items-center justify-center ">
-        <EmptyTitle className="text-5xl flex gap-2">
+      <EmptyHeader className="text-xl md:text-3xl lg:text-5xl h-screen flex items-center justify-center  ">
+        <EmptyTitle className=" text-5xl flex gap-2 font-semibold">
           <LinkBreakIcon size={48} /> 404
         </EmptyTitle>
         <Separator />
-        <EmptyTitle> عذراً لم يتم ايجاد ما تبحث عنه</EmptyTitle>
-        <EmptyDescription>الصفحة التي تبحث عنها غير موجودة</EmptyDescription>
-        <Button className="bg-[#dc7702]" variant="secondary" type="submit">
+        <EmptyTitle className="text-xl">
+          عذراً لم يتم ايجاد ما تبحث عنه
+        </EmptyTitle>
+        <EmptyDescription className="text-xl">
+          الصفحة التي تبحث عنها غير موجودة
+        </EmptyDescription>
+        <Button
+          className="bg-[#dc7602eb] text-lg py-5 px-2"
+          variant="secondary"
+          type="submit"
+        >
           <Link href="/"> العودة إلى الرئيسية</Link>
         </Button>
       </EmptyHeader>
